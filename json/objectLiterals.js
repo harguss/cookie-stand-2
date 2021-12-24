1cc55763ee782d5d80b8b706479046500e088e27
tektechnologies <info@tektechnologies.com>
2021-12-23 18:45:25 -0600

    add hourly custs to array

--- json/objectLiterals.js
+++ json/objectLiterals.js
@@ -2,11 +2,13 @@
 console.log('are you hearing the objectliteral js');
 
 // Stores the min/max hourly customers, and the average cookies per customer, in object properties
-// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
-// Display the values of each array as unordered lists in the browser
-// Calculating the sum of these hourly totals; your output for each location should look like this:
+// 1. () Uses a method of that object to generate a random number of customers per hour. 
+
+// 2.() Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+
+// 3. Need two Arrays === Store the results for each location in a separate array… perhaps as a property of the object representing that location
+// 4. Display the values of each array as unordered lists in the browser
+// 5. Calculating the sum of these hourly totals; your output for each location should look like this:
 
 
 
@@ -15,13 +17,29 @@ const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm',
 
 let firstStore = {
   storeName : 'Seattle',
-  custMaxHour : '65',
-  custMinHour : '23',
-  avgCookiePerSale :'6.3',
-};
+  custMaxHour : 65,
+  custMinHour : 23,
+  avgCookiePerSale : 6.3,
+  hourlyCustomers :  [],
+  hourlyCookies :   [],
+  numCustomersPerHour: function(){
+    for(let i = 0; i < hours.length; i++){
+      this.hourlyCustomers.push(random(this.custMinHour, this.custMaxHour));
+    }
+  }
+  // cookiesForEachHour : function(){
 
+  // }
+
+};
 console.log(firstStore);
 console.log('store hours list', hours);
+firstStore.numCustomersPerHour();
+
+
+function random(min, max){
+  return Math.round(Math.random() * (max - min) + min);
+}
 
 // custEachHour: [],
 // cookieEachHour: [],
@@ -38,36 +56,36 @@ console.log('store hours list', hours);
 
 
 let secondStore = {
-  storeName : 'Toykeo',
-  custMaxHour : '24',
-  custMinHour : '3',
-  avgCookiePerSale :'1.2',
+  storeName : 'Tokyo',
+  custMaxHour : 24,
+  custMinHour : 3,
+  avgCookiePerSale : 1.2,
 };
 console.log(secondStore);
 
 
 let thirdStore = {
   storeName : 'Dubai',
-  custMaxHour : '38',
-  custMinHour : '11',
-  avgCookiePerSale :'3.7',
+  custMaxHour : 38,
+  custMinHour : 11,
+  avgCookiePerSale : 3.7,
 };
 
 console.log(thirdStore);
 
 let forthStore = {
   storeName : 'Paris',
-  custMaxHour : '20',
-  custMinHour : '11',
-  avgCookiePerSale :'2.3',
+  custMaxHour : 20,
+  custMinHour : 11,
+  avgCookiePerSale : 2.3,
 };
 console.log(forthStore);
 
 let fifthStore = {
   storeName : 'Lima',
-  custMaxHour : '16',
-  custMinHour : '2',
-  avgCookiePerSale :'4.6',
+  custMaxHour :  16,
+  custMinHour : 2,
+  avgCookiePerSale : 4.6,
 };
 
 console.log(fifthStore);
