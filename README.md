f30d5b743911c8e68b0c4e9ceade98fcbbe6a5ed
SHargus <87950738+harguss@users.noreply.github.com>
2021-12-23 18:02:49 -0600

    css hero

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cookie-stand-2
+### Links and resourses
+*wave backgound attribution-<a href="https://www.vecteezy.com/free-vector/japanese-wave-pattern">Japanese Wave Pattern Vectors by Vecteezy</a>
--- json/objectLiterals.js
+++ json/objectLiterals.js
@@ -1,11 +1,13 @@
 'use strict';
 console.log('are you hearing the objectliteral js');
 
-// Pat’s Salmon Cookies, soon with franchises internationally, needs to calculate the number of cookies each location must make every day so that it can manage its supplies inventory and baking schedule. The number of cookies to make depends on the hours of operation (6:00 AM to 8:00 PM for all locations) and a few factors unique to each location:
+// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+// Store the results for each location in a separate array… perhaps as a property of the object representing that location
+// Display the values of each array as unordered lists in the browser
+// Calculating the sum of these hourly totals; your output for each location should look like this:
 
-// The minimum number of customers per hour.
-// The maximum number of customers per hour.
-// The average number of cookies purchased per customer.
 
 
 
@@ -17,7 +19,22 @@ let firstStore = {
   custMinHour : '23',
   avgCookiePerSale :'6.3',
 };
+
 console.log(firstStore);
+console.log('store hours list', hours);
+
+// custEachHour: [],
+// cookieEachHour: [],
+// totalDailyCookies: 0,
+// calcCustomersEachHour: function(){
+//   for(let i = 0; i < hours.length; i++){
+//     this.customersEachHour.push(random(this.minCustomerPerHour, this.maxCustomerPerHour));
+//   }
+// },
+
+
+
+
 
 
 let secondStore = {
