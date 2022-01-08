261abeabc3a9b462468280af4fbe3ab5b379b0bd
SHargus <87950738+harguss@users.noreply.github.com>
2022-01-08 16:40:45 -0600

    created new cookiestore constructor function

--- json/app.js
+++ json/app.js
@@ -1,3 +1,99 @@
 'use strict';
-console.log('is my js connected?');
+console.log('are you hearing the object literal js');
+
+
+
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+
+//constructor function
+function CookieStore(storeName,custMaxHour, custMinHour, avgCookiePerSale, hourlyCustomers, hourlyCookies){
+  this.storeName = storeName;
+  this.custMaxHour = custMaxHour;
+  this.custMinHour = custMinHour;
+  this.avgCookiePerSale = avgCookiePerSale;
+  this.hourlyCustomers = hourlyCustomers;
+  this.hourlyCookies = hourlyCookies;
+}
+
+
+//call new for each store
+let firstStore = new CookieStore('Seattle', 65, 23, 6.3, [], []);
+let secondStore = new CookieStore('Tokyo', 24, 3, 1.2, [], []);
+let thirdStore =  new CookieStore('Dubai', 38, 11, 3.7, [], []);
+let forthStore = new CookieStore('Paris', 20, 11, 2.3, [], []);
+let fifthStore = new CookieStore('Lima', 16, 2, 4.6, [], []);
+
+
+//store all the stores in an array to loop over once they have their data.
+let storeArray = [firstStore, secondStore, thirdStore, forthStore, fifthStore];
+console.log('storeArray', storeArray);
+
+// header method
+// render method
+// footer method
+
+
+
+
+
+
+
+
+//make prototype methods for our objects so they can so stuff.
+// numCustomersPerHour: function(){
+//   for(let i = 0; i < hours.length; i++){
+//     this.hourlyCustomers.push(random(this.custMinHour, this.custMaxHour));
+//   }
+// },
+// cookiesForEachHour : function(){
+//   // console.log('cookiesssss');
+//   for(let i = 0; i < hours.length; i++){
+//     this.hourlyCookies.push(Math.round(this.hourlyCustomers[i] * this.avgCookiePerSale));
+//   }
+// }
+
+
+// firstStore.numCustomersPerHour();
+// firstStore.cookiesForEachHour();
+
+// secondStore.numCustomersPerHour();
+// secondStore.cookiesForEachHour();
+
+// thirdStore.numCustomersPerHour();
+// thirdStore.cookiesForEachHour();
+
+// forthStore.numCustomersPerHour();
+// forthStore.cookiesForEachHour();
+
+// fifthStore.numCustomersPerHour();
+// fifthStore.cookiesForEachHour();
+
+
+
+
+function random(min, max){
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+
+//How do we access the DOM
+let ulElement = document.getElementById('storeList');
+// console.log('this is our ul: ',ulElement);
+
+
+// console.log('store Array',storeArray[0].hourlyCookies);
+
+for(let i = 0; i < storeArray.length; i++){
+  for(let j = 0; j < hours.length; j++){
+    console.log('inside for loop J: ', hours[j]);
+    // create need some li's
+    let hourlyLi = document.createElement('li');
+    // set the textContent to that li
+    hourlyLi.textContent = hours[j] + ' ' + storeArray[i].hourlyCookies[j];
+    // then we need to append that li to our ul variable ulElement
+    ulElement.appendChild(hourlyLi);
+
+  }
+}
 
