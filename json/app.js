1757041d3c62d37092db786129db9fa5de0fd8b1
SHargus <87950738+harguss@users.noreply.github.com>
2022-01-22 16:49:36 -0600

    fixed table render

--- json/app.js
+++ json/app.js
@@ -1,11 +1,10 @@
 'use strict';
 console.log('are you hearing the object literal js');
 
-
 //Declare Global variables!!!
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 let cookieTable = document.getElementById('storeList');
-let newStore;
+// let newStore;
 
 //declare constructor function-name should be capitolized!!!!!
 function CookieStore(storeName,custMaxHour, custMinHour, avgCookiePerSale, hourlyCustomers, hourlyCookies){
@@ -15,12 +14,22 @@ function CookieStore(storeName,custMaxHour, custMinHour, avgCookiePerSale, hourl
   this.avgCookiePerSale = avgCookiePerSale;
   this.hourlyCustomers = hourlyCustomers;
   this.hourlyCookies = hourlyCookies;
+  // PizzaPicture.allImages.push(this);
+  this.numCustomersPerHour();
+  this.cookiesForEachHour();
+  CookieStore.storeArray.push(this);
 }
+
+// PizzaPicture.allImages = [];
+CookieStore.storeArray = [];
+console.log(CookieStore.storeArray);
+
 //declare prototype methods
 CookieStore.prototype.numCustomersPerHour = function(){
   for(let i = 0; i < hours.length; i++){
     this.hourlyCustomers.push(random(this.custMinHour, this.custMaxHour));
   }
+
 };
 CookieStore.prototype.cookiesForEachHour = function(){
   for(let i = 0; i < hours.length; i++){
@@ -32,21 +41,11 @@ CookieStore.prototype.cookiesForEachHour = function(){
 function random(min, max){
   return Math.round(Math.random() * (max - min) + min);
 }
-
-
-
-
-
-
-//call new for each store
-let firstStore = new CookieStore('Seattle', 65, 23, 6.3, [], []);
-let secondStore = new CookieStore('Tokyo', 24, 3, 1.2, [], []);
-let thirdStore =  new CookieStore('Dubai', 38, 11, 3.7, [], []);
-let forthStore = new CookieStore('Paris', 20, 11, 2.3, [], []);
-let fifthStore = new CookieStore('Lima', 16, 2, 4.6, [], []);
-
-
-
+new CookieStore('Seattle', 65, 23, 6.3, [], []);
+new CookieStore('Tokyo', 24, 3, 1.2, [], []);
+new CookieStore('Dubai', 38, 11, 3.7, [], []);
+new CookieStore('Paris', 20, 11, 2.3, [], []);
+new CookieStore('Lima', 16, 2, 4.6, [], []);
 
 CookieStore.prototype.tableHeader = function(){
   let cookieTable = document.getElementById('storeList');
@@ -66,6 +65,7 @@ CookieStore.prototype.tableHeader = function(){
 };
 
 CookieStore.prototype.render = function() {
+  // cookieTable.innerHTML = '';
   let locRow = document.createElement('tr');
   let locationCell = document.createElement('td');
   locationCell.textContent = this.storeName;
@@ -85,6 +85,7 @@ CookieStore.prototype.render = function() {
 };
 
 CookieStore.prototype.footer = function(){
+
   let footerRow = document.createElement ('tr');
   let nameCell = document.createElement('td');
   nameCell.textContent = 'Totals';
@@ -93,137 +94,51 @@ CookieStore.prototype.footer = function(){
   let totalTally = 0;
   for(let i = 0; i < hours.length; i++){
     let hourlyTally = 0;
-    for(let j = 0; j < storeArray.length; j++){
+    for(let j = 0; j < CookieStore.storeArray.length; j++){
       // console.log(storeArray[j].hourlyCookies[i]);
-      hourlyTally += storeArray[j].hourlyCookies[i];
-      totalTally += storeArray[j].hourlyCookies[i];
+      hourlyTally += CookieStore.storeArray[j].hourlyCookies[i];
+      totalTally += CookieStore.storeArray[j].hourlyCookies[i];
     }
     let totalCell = document.createElement('td');
     totalCell.textContent = hourlyTally;
     footerRow.appendChild(totalCell);
   }//close
+
   let totalFinalCell = document.createElement('td');
   totalFinalCell.textContent = totalTally;
   footerRow.appendChild(totalFinalCell);
   cookieTable.appendChild(footerRow);
 };
 
-let storeArray = [firstStore, secondStore, thirdStore, forthStore, fifthStore];
-console.log('storeArray', storeArray);
-
-firstStore.numCustomersPerHour();
-firstStore.cookiesForEachHour();
-secondStore.numCustomersPerHour();
-secondStore.cookiesForEachHour();
-thirdStore.numCustomersPerHour();
-thirdStore.cookiesForEachHour();
-forthStore.numCustomersPerHour();
-forthStore.cookiesForEachHour();
-fifthStore.numCustomersPerHour();
-fifthStore.cookiesForEachHour();
-
-
-//Add event listners and handlers
 function handleFormSubmitted(event){
   event.preventDefault();
   let storeName = event.target.storeName.value;
-  let custMaxHour = event.target.custMaxHour.value;
-  let custMinHour = event.target.custMinHour.value;
-  let avgCookiePerSale = event.target.avgCookiePerSale.value;
-  newStore = new CookieStore(storeName, custMaxHour, custMinHour, avgCookiePerSale, [], []);
-  newStore.numCustomersPerHour();
-  newStore.cookiesForEachHour();
-  newStore.render();
-  let form = document.getElementById('formElement');
+  let custMaxHour = parseInt(event.target.custMaxHour.value);
+
+  let custMinHour = parseInt(event.target.custMinHour.value);
+  let avgCookiePerSale = parseInt(event.target.avgCookiePerSale.value);
+
+  new CookieStore(storeName, custMaxHour, custMinHour, avgCookiePerSale, [], []);
+
+  finalRender();
+
+  form = document.getElementById('formElement');
   form.reset();
-  //final render()
 }
-let formElement = document.getElementById('formElement');
-formElement.addEventListener('submit', handleFormSubmitted);
 
-
-//CallFunctions
-//You still need to do this
-//1.run the header function
-CookieStore.prototype.tableHeader();
-//2.then run stores render ();
-firstStore.render();
-secondStore.render();
-thirdStore.render();
-forthStore.render();
-fifthStore.render();
-//3. then we run the footer to get totals
-
-CookieStore.prototype.footer();
-var Table = document.getElementById("mytable");
- Table.innerHTML = "";
-
- function finalRender(){
-  cookieTable.innerHTML = '';
-
-//////////
-function handleFormSubmitted(event){
-  event.preventDefault();
-  // console.log('we made it to the event function');
-  // console.log('event',event.target.storeName.value);
-  // console.log('event',event.target.custMaxHour.value);
-  // console.log('event',event.target.custMinHour.value);
-  // console.log('event',event.target.avgCookiePerSale.value);
-
-  let storeName = event.target.storeName.value;
-  // console.log('location name', storeName);
-  let custMaxHour =event.target.custMaxHour.value;
-  // console.log('max customers in an hour', custMaxHour);
-  let custMinHour = event.target.custMinHour.value;
-  // console.log('Minimum customers in an hour', custMinHour);
-  let avgCookiePerSale = event.target.avgCookiePerSale;
-  // console.log('Average numbers of cookies bought', avgCookiePerSale);
-newStore = new CookieStore(storeName, custMaxHour, custMinHour, avgCookiePerSale, [], []);
-newStore.numCustomersPerHour();
-newStore.cookiesForEachHour();
-newStore,render();
 let form = document.getElementById('formElement');
-formElement.addEventListener('submit', 'handleFormSubmitted');
-/////////
+console.log(form);
+form.addEventListener('submit', handleFormSubmitted);
 
-// function finalRender(){
+function finalRender(){
+  cookieTable.innerHTML = '';
   CookieStore.prototype.tableHeader();
-
-   for(let i = 0; i < storeArray.length; i++){
-   storeArray[i].render();
-   }
-
-   CookieStore.prototype.footer();
-
-
-
-
-
-
-
-finalRender();
-
-  newStore = new CookieStore(storeName, custMaxHour, custMinHour, avgCookiePerSale, [], []);
-  console.log('new store', newStore);
-
-  newStore.numCustomersPerHour();
-  newStore.cookiesForEachHour();
-  newStore.render();
-
-  let form = document.getElementById('new-store');
-  form.reset();
-
-  // we re run all the stores from the storeArray
-//  finalRender();
+  for(let i = 0; i < CookieStore.storeArray.length; i++){
+    CookieStore.storeArray[i].render();
+  }
+  CookieStore.prototype.footer();
 }
 
-// console.log('form?????',formElement);
-let formElement = document.getElementById('formElement');
-formElement.addEventListener('submit', handleFormSubmitted);
-
-formElement.addEventListener('submit', function(event){
-  event.preventDefault();
-  // console.log('clickin on submit.');
-});
-
 finalRender();
+
+
